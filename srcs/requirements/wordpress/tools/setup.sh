34578bcea744436bbfcf6c3819ec38dce1372fc2
ryhara <ryosei.hara@gmail.com>
2023-11-30 18:59:10 +0900

    [Add] wordpress.tar.gz, wp-cli,, setup.sh

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,3 +1,10 @@
 #!/bin/baah
 
 echo "wordpress setup.sh"
+
+# tar -xvzf /tmp/wordpress6.4.1.tar.gz -C /var/www/html/
+# rm -rf /tmp/wordpress.tar.gz
+# mv wordpress/* /var/www/html/
+# rm -rf wordpress
+# groupadd -r wordpress && useradd -r -g wordpress wordpress
+# # chown -R wordpress:wordpress /var/www/html
