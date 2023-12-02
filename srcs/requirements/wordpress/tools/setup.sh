bc8f7cbb74c594b887bb7e6946c93702c9180a96
ryhara <ryosei.hara@gmail.com>
2023-12-02 11:16:21 +0900

    [Fix] setup.sh [Next] 最初にコマンドラインからuser登録をする

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-tar -xvzf /tmp/wordpress6.4.1.tar.gz -C /var/www/html/
-mv /var/www/html/wordpress/* /var/www/html/
+tar -xvzf /tmp/wordpress-6.4.1.tar.gz -C /var/www/html/
+mv /var/www/html/wordpress/* /var/www/html/.
 rm -rf /var/www/html/wordpress
+
+# wpの設定 wp command
+
 
 exec "$@"
