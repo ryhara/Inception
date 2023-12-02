553cc06214b70a2e980fe2ab9852c4a1c20d52bb
ryhara <ryosei.hara@gmail.com>
2023-12-02 18:30:51 +0900

    [Fix] adminer php -t

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -5,7 +5,27 @@ mv /var/www/html/wordpress/* /var/www/html/.
 rm -rf /var/www/html/wordpress
 mv /tmp/test.html /var/www/html/
 
-# wpの設定 wp command
+wp config create	--allow-root \
+					--dbname=$WORDPRESS_DB_NAME \
+					--dbuser=$WORDPRESS_DB_USER \
+					--dbpass=$WORDPRESS_DB_PASSWORD \
+					--dbhost=$WORDPRESS_DB_HOST \
+					--path=/var/www/html/ \
 
+wp core install		--allow-root \
+					--url=$WORDPRESS_URL \
+					--title=$WORDPRESS_TITLE \
+					--admin_user=$WORDPRESS_ADMIN_USER \
+					--admin_password=$WORDPRESS_ADMIN_PASSWORD \
+					--admin_email=$WORDPRESS_ADMIN_EMAIL \
+					--skip-email \
+					--path=/var/www/html/ \
+
+wp user create		--allow-root \
+					$WORDPRESS_USER1 \
+					$WORDPRESS_USER1_EMAIL \
+					--user_pass=$WORDPRESS_USER1_PASSWORD \
+					--role=author \
+					--path=/var/www/html/ \
 
 exec "$@"
