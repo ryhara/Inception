5d0c5dcb9cc74312214c05b538c63b8cfc01cf80
ryhara <ryosei.hara@gmail.com>
2023-12-02 16:54:41 +0900

    [Fix] COPY

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -3,6 +3,7 @@
 tar -xvzf /tmp/wordpress-6.4.1.tar.gz -C /var/www/html/
 mv /var/www/html/wordpress/* /var/www/html/.
 rm -rf /var/www/html/wordpress
+mv /tmp/test.html /var/www/html/
 
 # wpの設定 wp command
 
